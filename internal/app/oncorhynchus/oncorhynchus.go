d3e3d42b306a86ed16ef2405b7c0aef5637ad6c3
chiyoi <chiyoi2140@outlook.com>
2023-03-22 22:15:06 +0800

    update

--- internal/app/oncorhynchus/oncorhynchus.go
+++ internal/app/oncorhynchus/oncorhynchus.go
@@ -1,7 +1,7 @@
 package oncorhynchus
 
 import (
-	"github.com/chiyoi/oncorhynchus/internal/oncorhynchus/authenticator"
+	"github.com/chiyoi/oncorhynchus/internal/app/oncorhynchus/authenticator"
 	"github.com/chiyoi/oncorhynchus/pkg/sakana"
 )
 
@@ -12,7 +12,7 @@ Welcome to oncorhynchus!
 `
 
 func Handler() sakana.Handler {
-	c := sakana.NewCommandGroup("oncorhynchus", "oncorhynchus <command> ...", "cli app")
+	c := sakana.NewCommandGroup("oncorhynchus", "oncorhynchus <command> ...", "command-line app")
 	c.Welcome(welcome)
 
 	c.Command("authenticator", authenticator.Handler(), "interact with neko03authenticator")
