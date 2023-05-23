544aef6d97962b5809c4ca60a512423df50b35de
chiyoi <chiyoi2140@outlook.com>
2023-05-23 15:59:51 +0800

    update

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 Terminal applications.
 - oncorhynchus-masou: A little console game.
 - パイモン/派蒙: 最高の仲間！
-
 * trinity: The command-line interface for *Trinity* service.
--- internal/app/trinity/common/data/data.go
+++ internal/app/trinity/common/data/data.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 	"sync"
 
@@ -27,23 +28,31 @@ func Load() {
 	mu.Lock()
 	defer mu.Unlock()
 
+	logs.Info("loading data")
 	f, err := os.Open(config.PathData)
 	if err != nil {
 		if os.IsNotExist(err) {
 			return
 		}
-		logs.Panic(err)
+		logs.Error(err)
+		fmt.Println("Failed to load data.")
+		logs.Fatal("exit")
 	}
 	json.NewDecoder(f).Decode(&Data)
+	logs.Info("data loaded")
 }
 
 func Save() {
 	mu.RLock()
 	defer mu.RUnlock()
 
+	logs.Info("saving data")
 	f, err := os.Create(config.PathData)
 	if err != nil {
-		logs.Panic(err)
+		logs.Error(err)
+		fmt.Println("Failed to save data.")
+		logs.Fatal("exit")
 	}
 	json.NewEncoder(f).Encode(&Data)
+	logs.Info("data saved")
 }
