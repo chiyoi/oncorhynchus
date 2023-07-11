23453e6fa61cea79da84bea3e23a4302504b9814
chiyoi <chiyoi2140@outlook.com>
2023-07-11 05:13:37 +0000

    tidy

---
+++ scripts.sh
@@ -0,0 +1,33 @@
+usage() {
+    echo "scripts:"
+    echo "scripts.sh install"
+    echo "    Install the commands."
+    echo "scripts.sh path"
+    echo "    Show path to the directory where the commands installed."
+}
+
+install() {
+	test -d ~/bin || mkdir ~/bin
+	cp -f cmd/*  ~/bin
+	echo "Scripts installed~"
+	echo "Add to PATH:"
+    path
+}
+
+path() {
+    echo "$HOME/bin"
+}
+
+case $1 in
+""|-h|-help|--help)
+usage
+exit
+;;
+install|path)
+;;
+*)
+usage
+exit 1
+esac
+
+$@
